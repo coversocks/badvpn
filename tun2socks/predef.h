689cd9d5a399a25bfde1c2cfae647f848eaf7164
coversocks <covertlysocks@gmail.com>
2020-11-27 16:48:48 +0800

    add ios project.

---
+++ tun2socks/predef.h
@@ -0,0 +1,19 @@
+//
+//  predef.h
+//  tun2socks
+//
+//  Created by Centny on 2020/11/27.
+//
+
+#ifndef predef_h
+#define predef_h
+
+#define BADVPN_THREAD_SAFE 0
+#define BADVPN_FREEBSD
+#define BADVPN_BREACTOR_BADVPN
+#define _GNU_SOURCE
+#define BADVPN_USE_KEVENT
+#define BADVPN_LITTLE_ENDIAN
+#define UDPGW_EMBED
+
+#endif /* predef_h */
